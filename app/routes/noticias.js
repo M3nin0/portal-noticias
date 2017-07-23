23f504272dcf3888d9359e4ee67f186cae107b3e
Felipe Menino <efelipecarlos@gmail.com>
2017-07-23 20:14:30 -0300

    Adição da conexão com banco de dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,29 @@
 module.exports = function(app){
-	app.get("/noticias", function(req, res){
-		res.render("noticias/noticias");
+
+	app.get('/noticias', function(req, res){
+
+		//Importando modulo para conexão MySQL
+		var mysql = require('mysql');
+
+		//Fazendo a comunicação
+		var connection = mysql.createConnection({
+			host: '127.0.0.1',
+			user: 'root',
+			password: '123456',
+			database: 'portal_noticias'
+		});
+
+		connection.query('select * from noticias', function(erro, result){
+
+			//Dentro da função criada como parametro há dois parametros, erro e result
+			//O erro será preenchido quando algum erro ocorrer, e o result é onde
+			//fica armazenado o resultado da consulta feita
+
+			res.send(result);
+
+		});
+	
+		//res.render('noticias/noticias');
+
 	});
 };
